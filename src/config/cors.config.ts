7e095aadf16d8048f36584bba71dae96b69189c0
mazoku1999 <rogelio.carata10@gmail.com>
2025-01-18 17:00:11 -0400

    Se modifico cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,9 +2,13 @@ import { CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
     origin: [
+        'http://localhost:5173',
+        'http://localhost:4173',
         'http://localhost:3000',
         'https://motionsoundnews.com',
-        'https://api.motionsoundnews.com'
+        'https://www.motionsoundnews.com',
+        'https://api.motionsoundnews.com',
+        'https://admin.motionsoundnews.com'
     ],
     credentials: true
 }; 
